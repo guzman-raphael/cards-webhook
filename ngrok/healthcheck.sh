e058c3143e3426e306962be61d0d5980afcce86d
guzman-raphael <raphael.h.guzman@gmail.com>
2019-07-19 11:29:53 -0500

    Fix healthcheck bug.

--- ngrok/healthcheck.sh
+++ ngrok/healthcheck.sh
@@ -2,6 +2,6 @@
 
 # curl --fail http://localhost:4040/api/tunnels || exit 1
 
-NGROK_URL=$(curl --fail http://localhost:4040/api/tunnels | jq -r '.tunnels[] | select(.name|test("http")) | .public_url')
-RESP=$(curl --fail $NGROK_URL)
+NGROK_URL=$(curl http://localhost:4040/api/tunnels | jq -r '.tunnels[] | select(.name|test("http")) | .public_url')
+RESP=$(curl $NGROK_URL)
 echo $RESP | grep -iv expire
